936ab5e5017ad7848e75d246bc284382796757bd
krgirard33 <ken.girard@gmail.com>
2023-04-29 15:37:15 -0500

    Added in Gradle-Groovy, SpringBoot 3.0.6, SpringBootDevTools, SpringWeb, SpringDataJPA,MySQLDriver

---
+++ back-end/src/main/java/com/liftoffac3/inventorme/InventormeApplication.java
@@ -0,0 +1,13 @@
+package com.liftoffac3.inventorme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventormeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventormeApplication.class, args);
+	}
+
+}
