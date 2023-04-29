3eae0f2f742ded4d9bb4e2b58720625fbd1911de
Ballarja <JamesBallard87@hotmail.com>
2023-04-28 22:25:48 -0500

    added installed react-scripts and added Froms.js and Registrations.js

---
+++ front-end/src/components/Forms.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Form(){
+    return(
+        <>
+            <form>
+                <input text="email">Email</input>
+            </form>
+        </>
+    )
+}
---
+++ front-end/src/components/Screens/Registration.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Form from "../forms";
+
+export default function Registration(){
+    return(
+        <Form/>
+    )
+}
