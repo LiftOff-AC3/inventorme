de2294179ad99c3055a95a35f5cb4bc114fa0019
Ballarja <JamesBallard87@hotmail.com>
2023-04-29 23:42:55 -0500

    registration form has ability to print inputed values to console-JB

--- front-end/src/components/layout/Forms.js
+++ front-end/src/components/layout/Forms.js
@@ -6,32 +6,45 @@ import Button from 'react-bootstrap/Button';
 
 export default function Forms(){
   
-    const [inputValue, setInputValue] = useState("");
+    const [name, setName] = useState("");
+    const [company, setCompany] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
-    const handleClick =(event) =>{
-      setInputValue(event.target.value);
+    const handleClick = (e) => {
+      e.preventDefault();
       console.log("Clicked");
+      console.log("Submitted");
+      console.log(`Name: ${name}`);
+      console.log(`Company: ${company}`);
+      console.log(`Email: ${email}`);
+      console.log(`Password: ${password}`);
+      console.log(`Confirm Password: ${confirmPassword}`);
     }
+ 
 
     return(
     <Form onSubmit={handleClick}>
-      <Form.Group className="m-5">
-        <Form.Label htmlFor="name">Name: </Form.Label>
-        <Form.Control type="name" placeholder="John Doe" id="name" name="text"></Form.Control>
+      <Form.Group controlId="formName" className="m-5">
+        <Form.Label>Name: </Form.Label>
+        <Form.Control type="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="John Doe"></Form.Control>
       </Form.Group>  
-      <Form.Group className="m-5">
-        <Form.Label htmlFor="name">Company: </Form.Label>
-        <Form.Control type="name" placeholder="My Company" id="company" name="text"></Form.Control>
+      <Form.Group controlId="formCompany" className="m-5">
+        <Form.Label>Company: </Form.Label>
+        <Form.Control type="name" value={company} onChange={(e) => setCompany(e.target.value)} placeholder="My Company"></Form.Control>
       </Form.Group>
-      <Form.Group className="m-5">
-        <Form.Label htmlFor="email">Email: </Form.Label>
-        <Form.Control type="email" placeholder="email@gmail.com" id="email" name="email"></Form.Control>
+      <Form.Group controlId="formEmail" className="m-5">
+        <Form.Label>Email: </Form.Label>
+        <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="email@gmail.com"></Form.Control>
       </Form.Group>
-        <Form.Group className="m-5">
-        <Form.Label htmlFor="password">Password: </Form.Label>
-        <Form.Control type="password" placeholder="*********" id="password" name="email"></Form.Control>
-        <Form.Label htmlFor="password">Verify Password: </Form.Label>
-        <Form.Control type="password" placeholder="*********" id="verify-password" name="email"></Form.Control>
+        <Form.Group controlId="formPassword" className="m-5">
+        <Form.Label>Password: </Form.Label>
+        <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="*********"></Form.Control>
+      </Form.Group>
+      <Form.Group controlId="formConfirmPassword" className="m-5">
+        <Form.Label>Verify Password: </Form.Label>
+        <Form.Control type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} placeholder="*********"></Form.Control>
       </Form.Group>
       <Button className="mx-5" variant="primary" type="submit">
         Submit
