86314e3ee6f880a71f7136307333599917781279
Ballarja <JamesBallard87@hotmail.com>
2023-04-29 21:30:24 -0500

    updated for to htmlFor made all id's unique values to fix errors in dev tools-JB

--- front-end/src/components/layout/Forms.js
+++ front-end/src/components/layout/Forms.js
@@ -1,28 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 
+
 export default function Forms(){
-  return(
-    <Form>
+  
+    const [inputValue, setInputValue] = useState("");
+
+    const handleClick =(event) =>{
+      setInputValue(event.target.value);
+      console.log("Clicked");
+    }
+
+    return(
+    <Form onSubmit={handleClick}>
       <Form.Group className="m-5">
-        <Form.Label for="name">Name: </Form.Label>
+        <Form.Label htmlFor="name">Name: </Form.Label>
         <Form.Control type="name" placeholder="John Doe" id="name" name="text"></Form.Control>
       </Form.Group>  
       <Form.Group className="m-5">
-        <Form.Label for="name">Company: </Form.Label>
-        <Form.Control type="name" placeholder="My Company" id="name" name="text"></Form.Control>
+        <Form.Label htmlFor="name">Company: </Form.Label>
+        <Form.Control type="name" placeholder="My Company" id="company" name="text"></Form.Control>
       </Form.Group>
       <Form.Group className="m-5">
-        <Form.Label for="email">Email: </Form.Label>
+        <Form.Label htmlFor="email">Email: </Form.Label>
         <Form.Control type="email" placeholder="email@gmail.com" id="email" name="email"></Form.Control>
       </Form.Group>
         <Form.Group className="m-5">
-        <Form.Label for="password">Password: </Form.Label>
+        <Form.Label htmlFor="password">Password: </Form.Label>
         <Form.Control type="password" placeholder="*********" id="password" name="email"></Form.Control>
-        <Form.Label for="password">Verify Password: </Form.Label>
-        <Form.Control type="password" placeholder="*********" id="password" name="email"></Form.Control>
+        <Form.Label htmlFor="password">Verify Password: </Form.Label>
+        <Form.Control type="password" placeholder="*********" id="verify-password" name="email"></Form.Control>
       </Form.Group>
       <Button className="mx-5" variant="primary" type="submit">
         Submit
