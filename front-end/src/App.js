9395911e0f9de7db5b541e24b8a805eb1afbf78a
Jennifer Young <jlynnyoung1@gmail.com>
2023-04-30 20:28:03 -0500

    4 - TODO for app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Registration from "./components/screens/Registration";
 import Landing from "./components/screens/Landing";
 
+//TODO: add either hooks or booleans to show or hide certain components based on button clicked JY 
 function App() {
   return (
     <div>
