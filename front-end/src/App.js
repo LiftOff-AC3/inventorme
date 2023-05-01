7305da2c0cde35fb77fe9f279c151ab038b8d72d
LauraKnoblock <laura.e.knoblock@gmail.com>
2023-05-01 15:38:25 -0500

    4 landing-page added useState functionality lk

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,13 +3,25 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Registration from "./components/screens/Registration";
 import Landing from "./components/screens/Landing";
+import { useState } from 'react';
 
 
 function App() {
+  const [showRegistration, setShowRegistration] = useState(false);
+  const [showLanding, setShowLanding] = useState(true)
+
+  function handleCreateAccount() {
+    setShowRegistration(true);
+    setShowLanding(false);
+
+  }
   return (
     <div>
-    <Landing />
-    <Registration/>
+        {showRegistration ? (
+        <Registration />
+      ) : (
+        <Landing handleCreateAccount={handleCreateAccount} />
+      )}
     </div>
   );
 }
