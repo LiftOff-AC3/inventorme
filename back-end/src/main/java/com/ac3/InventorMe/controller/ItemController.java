a513bc45abd9f50ba011fd229122a947e14590c7
Nolan <ndietz2121@gmail.com>
2023-05-01 17:23:44 -0500

    added get mapping

--- back-end/src/main/java/com/ac3/InventorMe/controller/ItemController.java
+++ back-end/src/main/java/com/ac3/InventorMe/controller/ItemController.java
@@ -4,10 +4,10 @@ import com.ac3.InventorMe.model.Item;
 import com.ac3.InventorMe.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnResource;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -19,5 +19,10 @@ public class ItemController {
     @PostMapping("/item")
     Item newItem(@RequestBody Item newItem){
         return itemRepository.save(newItem);
+    }
+
+    @GetMapping("/items")
+    List<Item>getAllItems(){
+        return itemRepository.findAll();
     }
 }
