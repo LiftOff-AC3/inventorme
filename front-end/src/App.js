3fdda3431279f98c21e2c57df325295a3afca76b
Nolan <ndietz2121@gmail.com>
2023-04-30 19:44:19 -0500

    Item Form

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Registration from "./components/screens/Registration";
 import Landing from "./components/screens/Landing";
+import ItemForm from './components/layout/ItemForm';
 
 function App() {
   return (
     <div>
     <Landing />
     <Registration/>
+    <ItemForm />
     </div>
   );
 }
