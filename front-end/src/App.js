be64ecb57d519b4c7c9604251486d5b05033e1af
Tashe <tstefacek92@gmail.com>
2023-05-03 15:46:24 -0500

    8-WIP Render Login Screen with error TS

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,30 +2,47 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Registration from "./components/screens/Registration";
 import Landing from "./components/screens/Landing";
-//import LoginPage from "./components/screens/Login";
+import Login from "./components/screens/Login";
 import { useState } from 'react';
 
 //TODO: add either hooks or booleans to show or hide certain components based on button clicked JY 
 function App() {
+  const [showLogin, setShowLogin] = useState(false)
   const [showRegistration, setShowRegistration] = useState(false);
   const [showLanding, setShowLanding] = useState(true)
 
+  function handleLogin() {
+    setShowLogin(true);
+    setShowRegistration(false);
+    setShowLanding(false);
+  }
+
   function handleCreateAccount() {
     setShowRegistration(true);
+    setShowLogin(false);
     setShowLanding(false);
 
   }
   return (
-    <div>
-    <Landing />
-    <Registration/>
-        {showRegistration ? (
-        <Registration />
-      ) : (
-        <Landing handleCreateAccount={handleCreateAccount} />
-      )}
-    </div>
-  );
+      <div>
+          {showRegistration ? (
+          <Registration />
+        ) : (
+          <Landing handleCreateAccount={handleCreateAccount} />
+        )}
+
+      </div>
+    );
 }
 
 export default App;
+
+      /*
+        I can't figure out how to get this to run in the return statement,
+
+         {showLogin ? (
+         <LoginPage /> : (
+         <Landing handleLogin={handleLogin} />
+         )}
+
+      */
