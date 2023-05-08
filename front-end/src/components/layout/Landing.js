79909e10662d03df37f8e7c95b95d182489d4be2
LauraKnoblock <laura.e.knoblock@gmail.com>
2023-05-08 18:47:24 -0500

    Tests and some bootstrap added- SignUp.js removed

--- front-end/src/components/layout/Landing.js
+++ front-end/src/components/layout/Landing.js
@@ -12,29 +12,23 @@ const Landing = (props) => {
 
 
     return (
-           <div id='landing-page' style={{height:'100vh'}}>
-           <nav style={{margin: '10px'}}>
-              <img src='logo_transparent.png' alt='logo' style={{float: 'left', marginRight: '1em'}} ></img>
-                <h2 style={{float: 'left', margin:'25px'}}>Welcome To InventorMe!</h2>
-                <div style={{float: 'right', margin: '25px'}}>
-                    <button id='addItem' class ="glass" type = 'submit' style={{margin:'10px'}}>Add Item</button>
-
-
-                    <button id='login' class='glass' onClick={handleLogin} style={{margin:'10px'}}>Login</button>
-
-                    <button id ='createAccount' class="glass" onClick={handleCreateAccount} style={{margin:'10px'}}>Create Account</button>
-                </div>
-            </nav>
-
-                <h3 style={{paddingTop: '100px', textAlign:'center'}}>Create your own customizable inventory with InventorMe!</h3>
-                <p style={{textAlign:'center'}}>
+        <div>
+           <div id='landing-page'  className='bg-dark text-white p-5'>
+        
+              <img src='logo_transparent.png' alt='logo' className="float-start"></img>
+                <h3 className='float-start m-4'>Welcome To InventorMe!</h3>
+                
+                 <button className="float-end btn btn-warning" id='addItem'  type = 'submit' style={{margin:'10px'}}>Add Item</button>
+                 <button className=" float-end btn btn-primary"id='login' onClick={handleLogin} style={{margin:'10px'}}>Login</button>
+                 <button className="float-end btn btn-success" id ='createAccount'onClick={handleCreateAccount} style={{margin:'10px'}}>Create Account</button>
+                 </div>
+                <p className="bg-dark text-white p-5 text-center">
+                <h4>Create your own customizable inventory with InventorMe!</h4>
                     <li>Easily organize your inventory at the click of a button</li>
                     <li>Add item details with custom fields</li>
                     <li>Search inventory by name, category, quantity</li>
-
-                </p>
-        </div>
-
+                    </p>
+                </div>
     );
 };
 
