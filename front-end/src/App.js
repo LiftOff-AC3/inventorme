c4f79cef8974fc24985035e3b533d870e82aceb6
Tashe <tstefacek92@gmail.com>
2023-05-10 16:52:02 -0500

    Added navigation links to Landing.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,35 +1,13 @@
 import { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Registration from "./components/layout/Registration";
 import Landing from "./components/layout/Landing";
-import Login from "./components/layout/LoginPage";
+
 
 function App() {
-
-  const [showLogin, setShowLogin] = useState(false)
-  const [showRegistration, setShowRegistration] = useState(false);
-  const [showLanding, setShowLanding] = useState(true)
-
-  function handleLogin() {
-    setShowLogin(true);
-    setShowRegistration(false);
-    setShowLanding(false);
-  }
-
-  function handleCreateAccount() {
-    setShowRegistration(true);
-    setShowLogin(false);
-    setShowLanding(false);
-  }
-  
   return (
-      <>
-        { showLanding && <Landing handleCreateAccount= { handleCreateAccount} handleLogin={ handleLogin} /> }
-        { showRegistration && <Registration /> }
-        { showLogin && <Login/> }
-      </>
-    );
+    <Landing />
+  );
 }
 
 export default App;
--- front-end/src/components/layout/Landing.js
+++ front-end/src/components/layout/Landing.js
@@ -1,35 +1,53 @@
 import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Login from "./LoginPage";
+import Registration from "./Registration";
 
+export default function Landing(){
 
-const Landing = (props) => {
-    function handleCreateAccount() {
-        props.handleCreateAccount();
+    function Navigation() {
+      return (
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+             </li>
+          </ul>
+        </nav>
+      );
     }
-
-    function handleLogin() {
-        props.handleLogin();
-    }
-
 
     return (
+    <>
         <div>
-           <div id='landing-page'  className='bg-dark text-white p-5'>
-        
-              <img src='logo_transparent.png' alt='logo' className="float-start"></img>
-                <h3 className='float-start m-4'>Welcome To InventorMe!</h3>
-                
-                 <button className="float-end btn btn-warning" id='addItem'  type = 'submit' style={{margin:'10px'}}>Add Item</button>
-                 <button className=" float-end btn btn-primary"id='login' onClick={handleLogin} style={{margin:'10px'}}>Login</button>
-                 <button className="float-end btn btn-success" id ='createAccount'onClick={handleCreateAccount} style={{margin:'10px'}}>Create Account</button>
-                 </div>
-                <p className="bg-dark text-white p-5 text-center">
-                <h4>Create your own customizable inventory with InventorMe!</h4>
-                    <li>Easily organize your inventory at the click of a button</li>
-                    <li>Add item details with custom fields</li>
+          <BrowserRouter>
+            <Navigation />
+            <Routes>
+              <Route path="/" element={Landing} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Registration />} />
+            </Routes>
+        </BrowserRouter>
+        </div>
+         <div id='landing-page'  className='bg-dark text-white p-5'>
+            <img src='logo_transparent.png' alt='logo' className="float-start"></img>
+             <h3 className='float-start m-4'>Welcome To InventorMe!</h3>
+             <p className="bg-dark text-white p-5 text-center">
+             <h4>Create your own customizable inventory with InventorMe!</h4>
+                <li>Easily organize your inventory at the click of a button</li>
+                  <li>Add item details with custom fields</li>
                     <li>Search inventory by name, category, quantity</li>
                     </p>
-                </div>
+        </div>
+       </>
     );
+
+
 };
 
-export default Landing;
