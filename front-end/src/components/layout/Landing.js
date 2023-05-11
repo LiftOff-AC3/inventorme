1edc798c96988985c0f56dbbaddb644e28b739ae
Tashe <tstefacek92@gmail.com>
2023-05-11 18:18:58 -0500

    20 Refactored again to make code more readable, updated CSS, added dummy log out link that will require another story to make functional TS

--- front-end/src/components/layout/Landing.js
+++ front-end/src/components/layout/Landing.js
@@ -6,14 +6,18 @@ export default function Landing(){
     return (
     <>
          <div id='landing-page'  className='bg-dark text-white p-5'>
-            <img src='logo_transparent.png' alt='logo' className="float-start"></img>
-             <h3 className='float-start m-4'>Welcome To InventorMe!</h3>
+            <div id="landing-left-col">
+            <img id="logo" src='logo_transparent.png' alt='logo' className="float-start"></img>
+             </div>
+             <div id="landing-right-col">
+             <h3 className='text-center'>Welcome To InventorMe!</h3>
              <p className="bg-dark text-white p-5 text-center">
              <h4>Create your own customizable inventory with InventorMe!</h4>
                 <li>Easily organize your inventory at the click of a button</li>
                   <li>Add item details with custom fields</li>
                     <li>Search inventory by name, category, quantity</li>
                     </p>
+              </div>
         </div>
        </>
     );
