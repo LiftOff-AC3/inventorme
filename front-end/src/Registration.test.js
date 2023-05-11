b3bfb3604ee286aa1f1e34b5db09dbb5abf76a4d
Ballarja <JamesBallard87@hotmail.com>
2023-05-11 18:55:19 -0500

    17.2-Created Registration.test, added first test-JB

---
+++ front-end/src/Registration.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Registration from "./components/layout/Registration";
+import {render, screen} from "@testing-library/react"
+
+describe("Registration component", () => {
+    it("renders login component without crashing", () => {
+        render(<Registration />);
+      });
+
+    it("")
+})
+
+
+
+
