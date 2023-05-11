d4de6fc1bb9e1ceb7b342104c23786bf07d4f0f0
Tashe <tstefacek92@gmail.com>
2023-05-11 18:43:54 -0500

    Reformatted front-end files for readability

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,71 +1,110 @@
-import { BrowserRouter, Routes, Route, Link, useLocation } from 'react-router-dom';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Item from "./components/layout/CreateItemForm"
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useLocation
+} from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Item from "./components/layout/CreateItemForm";
 import Registration from "./components/layout/Registration";
 import Landing from "./components/layout/Landing";
 import Login from "./components/layout/LoginPage";
 
 function App() {
   function Navigation() {
-        const location = useLocation();
-        const isRegistrationPage = location.pathname === '/register';
-        const isLoginPage = location.pathname === '/login';
-        const isAddItemPage = location.pathname === '/add';
-        const isLandingPage = location.pathname === '/';
+    const location = useLocation();
+    const isRegistrationPage = location.pathname === "/register";
+    const isLoginPage = location.pathname === "/login";
+    const isAddItemPage = location.pathname === "/add";
+    const isLandingPage = location.pathname === "/";
 
-        const linkStyle = {
-        textDecoration: "none"
-        }
+    const linkStyle = {
+      textDecoration: "none"
+    };
 
-        return (
-          <nav>
-              <div>
-              {isLandingPage && (
-                 <div class="landing-nav">
-                    <Link class="landing-nav-link"  to="/login" style={linkStyle}>Login</Link>
-                    <Link class="landing-nav-link"  to="/register" style={linkStyle}>Register</Link>
-                    <Link class="landing-nav-link"  to="/add" style={linkStyle}>Add</Link>
-                 </div>
-              )}
+    return (
+      <nav>
+        <div>
+          {isLandingPage && (
+            <div class="landing-nav">
+              <Link class="landing-nav-link" to="/login" style={linkStyle}>
+                Login
+              </Link>
+              <Link class="landing-nav-link" to="/register" style={linkStyle}>
+                Register
+              </Link>
+              <Link class="landing-nav-link" to="/add" style={linkStyle}>
+                Add
+              </Link>
+            </div>
+          )}
 
-              {isLoginPage && (
-                 <div class="login-nav">
-                   <Link class="login-nav-link"  to="/" style={linkStyle}><img id="logo" src='logo_transparent.png' alt='logo' className="float-start"></img></Link>
-                   <Link class="login-nav-link"  to="/register" style={linkStyle}>Register</Link>
-                </div>
-              )}
+          {isLoginPage && (
+            <div class="login-nav">
+              <Link class="login-nav-link" to="/" style={linkStyle}>
+                <img
+                  id="logo"
+                  src="logo_transparent.png"
+                  alt="logo"
+                  className="float-start"
+                ></img>
+              </Link>
+              <Link class="login-nav-link" to="/register" style={linkStyle}>
+                Register
+              </Link>
+            </div>
+          )}
 
-              {isRegistrationPage && (
-                  <div class="register-nav">
-                     <Link class="register-nav-link"  to="/" style={linkStyle}><img id="logo" src='logo_transparent.png' alt='logo' className="float-start"></img></Link>
-                     <Link class="register-nav-link"  to="/login" style={linkStyle}>Login</Link>
-                   </div>
-              )}
+          {isRegistrationPage && (
+            <div class="register-nav">
+              <Link class="register-nav-link" to="/" style={linkStyle}>
+                <img
+                  id="logo"
+                  src="logo_transparent.png"
+                  alt="logo"
+                  className="float-start"
+                ></img>
+              </Link>
+              <Link class="register-nav-link" to="/login" style={linkStyle}>
+                Login
+              </Link>
+            </div>
+          )}
 
-              {isAddItemPage && (
-                   <div class="add-nav">
-                      <Link class="add-nav-link"  to="/" style={linkStyle}><img id="logo" src='logo_transparent.png' alt='logo' className="float-start"></img></Link>
-                      <Link class="add-nav-link"  to="" style={linkStyle}>Log Out</Link>
-                   </div>
-              )}
-              </div>
-          </nav>
-        );
-      }
+          {isAddItemPage && (
+            <div class="add-nav">
+              <Link class="add-nav-link" to="/" style={linkStyle}>
+                <img
+                  id="logo"
+                  src="logo_transparent.png"
+                  alt="logo"
+                  className="float-start"
+                ></img>
+              </Link>
+              <Link class="add-nav-link" to="" style={linkStyle}>
+                Log Out
+              </Link>
+            </div>
+          )}
+        </div>
+      </nav>
+    );
+  }
 
   return (
-  <div>
-            <BrowserRouter>
-              <Navigation />
-              <Routes>
-                <Route path="/" element={<Landing />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Registration />} />
-                <Route path="/add" element={<Item />} />
-              </Routes>
-          </BrowserRouter>
-   </div>
+    <div>
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Registration />} />
+          <Route path="/add" element={<Item />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- front-end/src/components/layout/LoginPage.js
+++ front-end/src/components/layout/LoginPage.js
@@ -1,38 +1,50 @@
 import React, { useState } from "react";
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-
-export default function Login(){
-
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   function validateForm() {
     return email.length > 0 && password.length > 0;
-
   }
-   function handleSubmit(event) {
-        event.preventDefault();
-   }
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
 
-    return(
+  return (
     <div className="Login">
-    <h1 className="m-5 text-center" >Log In</h1>
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="formEmail" className="m-5">
-        <Form.Label>Email: </Form.Label>
-        <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="email@gmail.com"></Form.Control>
-      </Form.Group>
+      <h1 className="m-5 text-center">Log In</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId="formEmail" className="m-5">
+          <Form.Label>Email: </Form.Label>
+          <Form.Control
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="email@gmail.com"
+          ></Form.Control>
+        </Form.Group>
         <Form.Group controlId="formPassword" className="m-5">
-        <Form.Label>Password: </Form.Label>
-        <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="*********"></Form.Control>
-      </Form.Group>
-      <Button id = 'login-submit-button' className="mx-5" variant="primary" type="submit" disabled={!validateForm()}>
-        Submit
-      </Button>
-    </Form>
+          <Form.Label>Password: </Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="*********"
+          ></Form.Control>
+        </Form.Group>
+        <Button
+          id="login-submit-button"
+          className="mx-5"
+          variant="primary"
+          type="submit"
+          disabled={!validateForm()}
+        >
+          Submit
+        </Button>
+      </Form>
     </div>
-  )
+  );
 }
