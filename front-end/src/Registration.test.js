1df45b0316ce5acf256f1b8ebeaa0d1047526723
Ballarja <JamesBallard87@hotmail.com>
2023-05-11 20:10:14 -0500

    17.2-Creation of second test for Registration page-JB

--- front-end/src/Registration.test.js
+++ front-end/src/Registration.test.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Registration from "./components/layout/Registration";
-import {render, screen} from "@testing-library/react"
+import {render, fireEvent} from "@testing-library/react";
 
 describe("Registration component", () => {
     it("renders login component without crashing", () => {
-        render(<Registration />);
+        render(<Registration/>);
       });
 
-    it("")
+    it("Loads form correctly to page", () =>{
+        const { getByLabelText, getByText } = render(<Registration/>);
+
+        expect(getByLabelText("Name:")).toBeInTheDocument();
+        expect(getByLabelText("Company:")).toBeInTheDocument();
+        expect(getByLabelText("Email:")).toBeInTheDocument();
+        expect(getByLabelText("Password:")).toBeInTheDocument();
+        expect(getByLabelText("Verify Password:")).toBeInTheDocument();
+        expect(getByText("Submit")).toBeInTheDocument();
+    });
 })
 
 
