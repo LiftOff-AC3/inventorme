ac64af6addd0d548b7f097cab1afae182dedf024
Ballarja <JamesBallard87@hotmail.com>
2023-05-11 21:22:52 -0500

    17.2-Tests commented out to be saved for later-JB

--- front-end/src/Registration.test.js
+++ front-end/src/Registration.test.js
@@ -1,23 +1,27 @@
-import React from "react";
-import Registration from "./components/layout/Registration";
-import {render, fireEvent} from "@testing-library/react";
+// import React from "react";
+// import Registration from "./components/layout/Registration";
+// import {render, fireEvent} from "@testing-library/react";
 
-describe("Registration component", () => {
-    it("renders login component without crashing", () => {
-        render(<Registration/>);
-      });
+// describe("Registration component", () => {
+//     it("renders login component without crashing", () => {
+//         render(<Registration/>);
+//       });
 
-    it("Loads form correctly to page", () =>{
-        const { getByLabelText, getByText } = render(<Registration/>);
+//     it("Loads form correctly to page", () => {
+//         const { getByLabelText, getByText } = render(<Registration/>);
 
-        expect(getByLabelText("Name:")).toBeInTheDocument();
-        expect(getByLabelText("Company:")).toBeInTheDocument();
-        expect(getByLabelText("Email:")).toBeInTheDocument();
-        expect(getByLabelText("Password:")).toBeInTheDocument();
-        expect(getByLabelText("Verify Password:")).toBeInTheDocument();
-        expect(getByText("Submit")).toBeInTheDocument();
-    });
-})
+//         expect(getByLabelText("Name:")).toBeInTheDocument();
+//         expect(getByLabelText("Company:")).toBeInTheDocument();
+//         expect(getByLabelText("Email:")).toBeInTheDocument();
+//         expect(getByLabelText("Password:")).toBeInTheDocument();
+//         expect(getByLabelText("Verify Password:")).toBeInTheDocument();
+//         expect(getByText("Submit")).toBeInTheDocument();
+//     });
+
+//     it("", () => {
+
+//     });
+// })
 
 
 
