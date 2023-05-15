04a3a8907a566cd11ff1653339af57494bc36ae2
Jennifer Young <jlynnyoung1@gmail.com>
2023-05-15 17:14:34 -0500

    rename files and remove folder

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,11 +5,11 @@ import {
 } from "react-router-dom";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Item from "./components/layout/CreateItemForm";
+import Item from "./components/layout/CreateItem";
 import ItemsList from "./components/layout/ItemsList";
 import Registration from "./components/layout/Registration";
 import Landing from "./components/layout/Landing";
-import Login from "./components/layout/LoginPage";
+import Login from "./components/layout/Login";
 import Navigation from "./components/layout/Navigation"
 
 function App() {
