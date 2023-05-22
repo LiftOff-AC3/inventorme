2143cd4255ed72cfdc9a66f9d1893bf9678da2bf
Ballarja <JamesBallard87@hotmail.com>
2023-05-21 22:45:25 -0500

    1-created form to be able to update items-JB

--- back-end/src/main/java/com/ac3/InventorMe/controller/ItemController.java
+++ back-end/src/main/java/com/ac3/InventorMe/controller/ItemController.java
@@ -1,4 +1,5 @@
 package com.ac3.InventorMe.controller;
+
 import com.ac3.InventorMe.model.Item;
 import com.ac3.InventorMe.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
-
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -22,7 +22,7 @@ public class ItemController {
     }
 
     @PostMapping("/item")
-    Item newItem(@RequestBody Item newItem){
+    Item newItem(@RequestBody Item newItem) {
         return itemRepository.save(newItem);
     }
 
@@ -41,7 +41,7 @@ public class ItemController {
     }
 
     @GetMapping("/items")
-    List<Item>getAllItems(){
+    List<Item> getAllItems() {
         return itemRepository.findAll();
     }
 
