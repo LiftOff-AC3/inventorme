26b0ec57658e05355d5ff9754c9f98d61acc04f5
Tashe <tstefacek92@gmail.com>
2023-05-23 15:30:05 -0500

    Adds password hashing upon registration form completed and removes password and email columns rom RegistrationData table

--- back-end/src/main/java/com/ac3/InventorMe/model/RegistrationData.java
+++ back-end/src/main/java/com/ac3/InventorMe/model/RegistrationData.java
@@ -9,8 +9,6 @@ public class RegistrationData {
     private UUID uuid = UUID.randomUUID();
     private String name;
     private String company;
-    private String email;
-    private String password;
 
     public String getName() {
         return name;
@@ -27,23 +25,6 @@ public class RegistrationData {
     public void setCompany(String company) {
         this.company = company;
     }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
 
     public UUID getUuid() {
         return uuid;
