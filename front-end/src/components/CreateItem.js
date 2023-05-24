bf3b024cd2a5acd768803c86e70f7c1abd6d51df
Ballarja <JamesBallard87@hotmail.com>
2023-05-23 21:32:45 -0500

    1-Update to PUTMapping in itemController and updates to Axios Get and PUT methods in UpateItem-JB

--- front-end/src/components/CreateItem.js
+++ front-end/src/components/CreateItem.js
@@ -12,6 +12,7 @@ export default function CreateItem() {
   const onChangeItem = (e) => {
     setItem({ ...item, [e.target.name]: e.target.value });
   };
+
   const onSubmit = async (e) => {
     e.preventDefault();
     await axios.post("http://localhost:8080/item", item);
