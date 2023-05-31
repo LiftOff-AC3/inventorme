23f2098f243d2d6a9318eeea1ba10877fb9b8bfa
Ballarja <JamesBallard87@hotmail.com>
2023-05-30 23:13:48 -0500

    3-Added Delete fucntion back to dev-JB

--- front-end/src/components/ItemsList.js
+++ front-end/src/components/ItemsList.js
@@ -5,6 +5,7 @@ import UpdateItem from "./UpdateItem";
 export default function ItemsList() {
   const [items, setItems] = useState(null);
   const [error, setError] = useState(null);
+  const [selectedItems, setSelectedItems] = useState([]);
   const [open, setOpen] = useState({ update: false, id: null });
   const handleClose = () => {
     setOpen({ update: false, id: null });
@@ -22,13 +23,52 @@ export default function ItemsList() {
       });
   }, []);
 
-  if (error) return "Error: $ {error.message}";
-  if (!items) return "No items";
+  function handleCheckboxSelect(e, itemId) {
+    const isChecked = e.target.checked;
+    if (isChecked) {
+      setSelectedItems([...selectedItems, itemId]);
+    } else {
+      setSelectedItems(selectedItems.filter((id) => id !== itemId));
+    }
+  }
 
+  async function handleDelete(e) {
+    e.preventDefault();
+    try {
+      //deletes each item
+      await Promise.all(
+        selectedItems.map((itemId) =>
+          axios.delete("/item", {
+            data: { id: itemId },
+          })
+        )
+      );
+      //refreshes item table after deletion
+      axios
+        .get("/items")
+        .then((response) => {
+          setItems(response.data);
+        })
+        .catch((error) => {
+          setError(error);
+        });
+      //clear selected items array
+      setSelectedItems([]);
+    } catch (error) {
+      alert("Error deleting items", error);
+    }
+  }
   return (
     <>
       <h1 className="m-5 text-center">Items List</h1>
       <div className="item-list-table m-5">
+        <button
+          onClick={handleDelete}
+          type="button"
+          class="btn btn-secondary btn-sm btn-danger"
+        >
+          Delete
+        </button>
         <table className="table table-bordered table-striped ">
           <thead>
             <tr className="table-dark text-center">
@@ -43,6 +83,15 @@ export default function ItemsList() {
               items.length > 0 &&
               items.map((item) => (
                 <tr key={item.id}>
+                  <td>
+                    <label>
+                      <input
+                        type="checkbox"
+                        onChange={(e) => handleCheckboxSelect(e, item.id)}
+                        checked={selectedItems.includes(item.id)}
+                      />
+                    </label>
+                  </td>
                   <td> {item.id} </td>
                   <td> {item.itemName} </td>
                   <td> {item.itemQuantity} </td>
