dedcf23e04677f57884b889334f7c772624bb517
LauraKnoblock <laura.e.knoblock@gmail.com>
2023-06-01 15:36:35 -0500

    progress made on page, still need to work on phone accessibility and making the h4 and UL's work better -lk

--- front-end/src/components/Landing.js
+++ front-end/src/components/Landing.js
@@ -1,19 +1,28 @@
+import styles from "./Landing.css"
+import {Link} from "react-router-dom";
+
 export default function Landing() {
   return (
     <>
-      <div id="landing-page" className="bg-dark text-white p-5">
+      <div id="landing-page" className="p-5">
         <div id="landing-left-col">
           <img
             id="logo"
             src="logo_transparent.png"
             alt="logo"
-            className="float-start"
           ></img>
-        </div>
-        <div id="landing-right-col">
-          <h3 className="text-center">Welcome To InventorMe!</h3>
-          <p className="bg-dark text-white p-5 text-center">
-            <h4>Create your own customizable inventory with InventorMe!</h4>
+        
+        
+          <h3 className="text-center">Welcome To <strong>InventorMe<e>.</e></strong></h3>
+          </div>
+          <Link to="/register">
+          <button id="get-started-button" className="btn btn-warning mx-5" type="submit">
+            Get Started!
+          </button>
+          </Link>
+          <div id="landing-right-col" className="landing-content">
+          <p className="p-5 text-center">
+            <h4>Create your own customizable inventory with InventorMe.</h4>
             <li>Easily organize your inventory at the click of a button</li>
             <li>Add item details with custom fields</li>
             <li>Search inventory by name, category, quantity</li>
