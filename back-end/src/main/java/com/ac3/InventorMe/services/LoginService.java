32fb369fa14b31d37163e0a0c92780cf13aea7b1
Jennifer Young <jlynnyoung1@gmail.com>
2023-06-02 11:13:03 -0500

    Revert "Merge pull request #26 from LiftOff-AC3/18.1-encrypt-login"
    This reverts commit 0f3f0a0db978a3856fa54292783382c944b3f30e, reversing
    changes made to 4c96ea272f6922e8c0a1c45bc979a6d091493095.

--- back-end/src/main/java/com/ac3/InventorMe/services/LoginService.java
+++ back-end/src/main/java/com/ac3/InventorMe/services/LoginService.java
@@ -4,22 +4,17 @@ import com.ac3.InventorMe.model.Login;
 import com.ac3.InventorMe.repository.LoginRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Service
 public class LoginService {
     @Autowired
     private LoginRepository loginRepository;
-    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
     public boolean validateLogin(String email, String password) {
         Login login = loginRepository.findByEmail(email);
-        String enteredPassword = login.getPassword();
-
-        if (login != null && encoder.matches(password, enteredPassword))  {
+        if (login != null && password.equals(login.getPassword())) {
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 }
