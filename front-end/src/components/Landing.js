13d5c999bfe4a653ce4efa8a8bbab350db5c07fd
LauraKnoblock <laura.e.knoblock@gmail.com>
2023-06-08 17:03:06 -0500

    Landing page designed with change in logo color.

--- front-end/src/components/Landing.js
+++ front-end/src/components/Landing.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 export default function Landing() {
   return (
     <>
-      <div id="landing-page" className="p-5">
+      <div id="landing-page" className="p-5 container-md">
         <div id="landing-left-col">
           <img
             id="logo"
@@ -14,19 +14,24 @@ export default function Landing() {
         
         
           <h3 className="text-center">Welcome To <strong>InventorMe<e>.</e></strong></h3>
+        
           </div>
+          <div id="landing-right-col" className="landing-content">
+          <p>
+            <h4>Create your own customizable inventory with InventorMe.</h4>
+            <ul className="box-container">
+              <section className="box-row">
+                <li id="box1">Easily organize your inventory at the click of a button</li>
+                <li id="box2">Add item details with custom fields</li>
+                <li id="box3">Search inventory by name, category & quantity</li>
+                </section>
+            </ul>
+          </p>
           <Link to="/register">
           <button id="get-started-button" className="btn btn-warning mx-5" type="submit">
             Get Started!
           </button>
           </Link>
-          <div id="landing-right-col" className="landing-content">
-          <p className="p-5 text-center">
-            <h4>Create your own customizable inventory with InventorMe.</h4>
-            <li>Easily organize your inventory at the click of a button</li>
-            <li>Add item details with custom fields</li>
-            <li>Search inventory by name, category, quantity</li>
-          </p>
         </div>
       </div>
     </>
