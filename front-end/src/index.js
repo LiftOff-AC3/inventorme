52b3ead76998a8971366a94f25e7d2588353156c
Tashe <tstefacek92@gmail.com>
2023-06-12 11:11:38 -0500

    Adds Aws Amplify functionality TS

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Amplify} from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+import {AmplifyProvider} from "@aws-amplify/ui-react"
+import awsExports from "./aws-exports";
+
+Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AmplifyProvider>
+      <App />
+    </AmplifyProvider>
   </React.StrictMode>
 );
 
